5001699097aa2830cd97682e44d94008875c30d7
RioRifaldi99 <138589544+RioRifaldi99@users.noreply.github.com>
2023-10-31 11:51:47 +0700

    Add files via upload

---
+++ riorifaldi_202310311126.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.riorifaldi ("nama handphone",ram,memori,chipset,layar,harga,baterai,alternatif,"nilai akhir","rank") VALUES
+	 ('Xiaomi Redmi 10A','6GB','128GB','MediaTek Helio G25','IPS LCD 6.5 inci',2000000,'5000 mAh',2.5419512,0.121828474,2),
+	 ('Realme C25','4GB','64GB','MediaTek Helio G70','IPS LCD 6.5 inci',2000000,'6000 mAh',2.4824755,0.11897798,3),
+	 ('samsung Galaxy A02','3GB','32GB','MediaTek MT6739','PLS TFT 6.5 inci',1500000,'5000 mAh',1.6043122,0.07689011,8),
+	 ('Vivo Y12s','3GB','32GB','MediaTek Helio P35','IPS LCD 6.51 inci',1000000,'5000 mAh',1.122462,0.053796407,10),
+	 ('Oppo A15','3GB','32GB','MediaTek Helio P35','IPS LCD 6.52 inci',1500000,'4230 mAh',1.6654078,0.07981825,7),
+	 ('Infinix Hot 11S','8GB','128GB','MediaTek Helio G88','IPS LCD 6.78 inci',2500000,'5000 mAh',4.033121,0.193296,1),
+	 ('Tecno Spark 8','3GB','32GB','MediaTek Helio P22','IPS LCD 6.6 inci',2000000,'5000 mAh',1.8415906,0.08826219,6),
+	 ('Nokia C30','3GB','32GB','Unisoc SC9863A','IPS LCD 6.82 inci',1300000,'6000 mAh',2.0163085,0.09663592,5),
+	 ('Motorola Moto G20','4GB','64GB','MediaTek Helio G35','IPS LCD 6.5 inci',2500000,'5000 mAh',2.0801282,0.09969462,4),
+	 ('Lenovo K13','3GB','32GB','MediaTek Helio A22','IPS LCD 6.5 inci',1500000,'5000 mAh',1.4772432,0.07080005,9);
